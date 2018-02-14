261efb3648225d9b2ebd9d4660cb215345696c06
hillr <hillr@LR064W92-1718.ellis.int>
2018-02-14 15:07:19 -0500

    first stages.

--- src/Die.java
+++ src/Die.java
@@ -1,4 +1,28 @@
+import javax.swing.JOptionPane;
 
 public class Die {
+	private int numOfSides;
+	
+	// 
+	//constructor (takes a number as an input)
+	//roll (generates a random roll)
+	
+	public Die(int n){
+		String input = JOptionPane.showInputDialog("Dice come with different number of sides. This game can be played with "
+				+ "4-sided, 6-sided, 8-sided, 10-sided, 12-sided, and 20-sided dice. "
+				+ "Please enter the number that corresponds"
+				+ "with the number of sides your desired dice have.");
+		n = Integer.parseInt(input);
+		if (n == 4 || n == 6 ||n == 8 || n == 10 ||n == 12 ||n == 20 ){
+			int roll = (int)(Math.random()*n) + 1;
+			roll = n * 2;
+		}
+		else {
+			System.out.println("Invalid number of sides. Please enter either 4, 6, 8, 10, 12, or 20.");
+		}
+			
+	}		
+	
+
 
 }
--- src/LoadedDie.java
+++ src/LoadedDie.java
@@ -1,4 +1,32 @@
 
-public class LoadedDie {
+public class LoadedDie extends Die {
+
+	public LoadedDie(int n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+		
+		
+	double randNum = Math.random();
+	double num = randNum * 100;
+		
+	public int generateRoll() {
+		int something = (int)(Math.random() * 100) + 1;
+		if (something <= 80) {
+			int allNumRoll = (int)(Math.random()*/*getSideNum*/n()) + 1;
+			int roll = allNumRoll *2 - 1;
+			return roll;	
+			
+		}
+		else {
+			int allNumRoll = (int)(Math.random()* /*getSideNum*/ n()) + 1;
+			int roll = allNumRoll *2;
+			return roll;
+		}
+	}
+	
+	
+	
+
 
 }
